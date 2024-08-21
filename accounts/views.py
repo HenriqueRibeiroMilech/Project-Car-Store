7e28e4538e5511a82b4dfc88001f93f4e182df0b
Henrique Milech <devhenriquemilech@gmail.com>
2024-08-21 19:41:56 -0300

    add login/register/logout

---
+++ accounts/views.py
@@ -0,0 +1,41 @@
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm   #Importação necessaria para fazer esse sistema
+from django.shortcuts import render, redirect                     #Para criar Usuarios (UserCreationForm) e para logar (AuthenticationForm)
+from django.contrib.auth import authenticate, login, logout
+
+def register_view(request):
+    if request.method == 'POST': #Verifica se o http ta mandando um post(enviando algo para o sistema)
+        user_form = UserCreationForm(request.POST) #adiciona o Conteudo do formulario de criaçao de conta nessa variavel
+
+        if user_form.is_valid():  #se o formulario for valido(atende todos os requisitos da validação)
+            user_form.save() #ele envia para o banco de dados
+            return redirect('login') #e redireciona o usuario para a pagina login
+        
+    else:   #Se o http for um Get(ou seja estiver somente querendo exibir a pagina)
+        user_form = UserCreationForm() #Ele cria um Formulario pronto do django para criar ususarios vazio
+
+    return render(request, 'register.html', {'user_form': user_form}) #depois ele renderiza o nosso template
+
+
+
+def login_view(request):
+    if request.method == 'POST': #Verifica se o http ta mandando um post(enviando algo para o sistema)
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password) #verifica se o usuario e senha está correto e salva na variavel (import authenticate)
+
+        if user is not None: # se o usuario não for nulo
+            login(request, user) #faz o login
+            return redirect('cars_list')
+
+        else: # se não
+            login_form = AuthenticationForm() #retorna um formulario vazio (import login)
+
+    else:   #Se o http for um Get(ou seja estiver somente querendo exibir a pagina)
+        login_form = AuthenticationForm() #retorna um formulario vazio
+
+    return render (request, 'login.html', {'login_form': login_form})
+
+
+def logout_view(request):
+    logout(request) #faz o logout da conta
+    return redirect('cars_list') #retorna para a tela de carros
--- app/urls.py
+++ app/urls.py
@@ -4,9 +4,13 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from cars.views import cars_view, new_car_view
+from accounts.views import register_view, login_view, logout_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cars/', cars_view, name='cars_list'),
-    path('new_car/', new_car_view, name='new_car')
+    path('cars/', cars_view, name='cars_list'), #Url da pagina que mostra os carros
+    path('register/', register_view, name='register'), #Url da pagina para se registrar
+    path('login/', login_view, name='login'),   #Url da pagina para logar
+    path('logout/', logout_view, name='logout'),   #Url da pagina para deslogar
+    path('new_car/', new_car_view, name='new_car'),   #Url da pagina para criar novos carros
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
